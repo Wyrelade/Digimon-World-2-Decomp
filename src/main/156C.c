3c191205a933118eec535baad56ce103a2684f54
Wyrelade <wyrelade@gmail.com>
2026-09-19 23:45:44 +0300

    matched func_80032844 (s16 wrapper; prologue_save_hoist + padnop tail)

--- src/main/156C.c
+++ src/main/156C.c
@@ -3419,7 +3419,7 @@ INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_800326A4);
 
 void func_80032820(s16 arg0) { func_800326A4(arg0); }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80032844);
+void func_80032844(s16 arg0) { func_800326A4(arg0); }
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80032874);
 
