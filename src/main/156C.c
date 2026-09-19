71657fade442fa6ac168046b946ac2021674dc8b
Wyrelade <wyrelade@gmail.com>
2026-09-19 23:28:57 +0300

    matched func_800304F4 (void wrapper -> tail call; epilogue_unfill moves the sp dealloc before jr)

--- src/main/156C.c
+++ src/main/156C.c
@@ -3274,7 +3274,7 @@ INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_800304B4);
 
 void func_800304D4(void) { func_8002F318(); }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_800304F4);
+void func_800304F4(void) { func_8002F598(); }
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80030514);
 
