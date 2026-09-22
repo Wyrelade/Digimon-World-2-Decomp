2719082ff2109223fb4a8297c52b5a6ba68c813b
Wyrelade <wyrelade@gmail.com>
2026-09-22 22:46:46 +0300

    matched func_800343C4 (handler struct-by-value arg via epilogue_unfill+padnop)

--- src/main/156C.c
+++ src/main/156C.c
@@ -100,6 +100,7 @@ extern void func_800294AC();
 extern void func_80030D34();
 extern Cmd62C18 D_80062C18;
 extern void func_80036514(s16);
+extern void func_800363A4(s16);
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80010D6C);
 
@@ -3558,7 +3559,9 @@ void func_80034394(s16 arg0, s16 arg1, s16 arg2, HandlerArg arg) {
     func_80036514((u8)arg.field_24);
 }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_800343C4);
+void func_800343C4(s16 arg0, s16 arg1, s16 arg2, HandlerArg arg) {
+    func_800363A4((u8)arg.field_24);
+}
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_800343F4);
 
