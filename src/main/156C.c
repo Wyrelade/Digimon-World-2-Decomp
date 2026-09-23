7fbfd59c9fb777a5930e714d5c4aa4ec720f691b
Wyrelade <wyrelade@gmail.com>
2026-09-23 21:02:48 +0300

    matched func_80033CE4 (handler: query pos, store byte arg, run pair via nosplit+padnop)

--- src/main/156C.c
+++ src/main/156C.c
@@ -4734,7 +4734,16 @@ void func_80033C24(s16 a0, s16 a1, s16 a2, Arg33 d) {
     func_80036594(a0, a1, a2, &d);
 }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80033CE4);
+void func_80033CE4(s16 a0, s16 a1, s16 a2, HandlerArg d) {
+    Out33B24 buf;
+    s32 v = (u8)d.field_24;
+
+    func_80036164(a0, a1, a2, &d);
+    func_80033B24(d.field_10, d.field_12, &buf);
+    buf.field_2 = v;
+    func_80033B80(&buf, &d.field_10, &d.field_12);
+    func_80036594(a0, a1, a2, &d);
+}
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80033D94);
 
