97bc9d02fd907e36ac4d063c886a46d703a16563
Wyrelade <wyrelade@gmail.com>
2026-09-23 21:02:50 +0300

    matched func_80027044 (copy bytes, return dst or null via nosplit+padnop)

--- src/main/156C.c
+++ src/main/156C.c
@@ -3528,7 +3528,20 @@ INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80027004);
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80027014);
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80027044);
+u8 *func_80027044(u8 *dst, u8 *src, s32 n) {
+    u8 *r = 0;
+
+    if (dst != 0) {
+        u8 *d = dst;
+
+        while (n > 0) {
+            *dst++ = *src++;
+            n--;
+        }
+        r = d;
+    }
+    return r;
+}
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/156C", func_80027084);
 
